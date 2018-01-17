29f87c7121fbb1356b17f96753c68f608ac2d97f
Forix <forixmehdi@gmail.com>
2018-01-17 07:58:26 +0000

    Ajout du scrolling automatique

--- src/ma/forix/main/game/Game.java
+++ src/ma/forix/main/game/Game.java
@@ -1,24 +1,35 @@
 package ma.forix.main.game;
 
+import ma.forix.main.Component;
 import ma.forix.main.game.level.Level;
+import org.lwjgl.opengl.GL11;
 
 public class Game {
 
     Level level;
 
+    public static float xScroll, yScroll;
+
     public Game(){
-        level = new Level(10, 10);
+        level = new Level(Component.width/16, Component.height/16);
     }
 
     public void init(){
         level.init();
     }
 
+    public void translateView(float xa, float ya){
+        xScroll+=xa;
+        yScroll+=ya;
+    }
+
     public void update(){
+        translateView(-0.1f,-0.1f);
         level.update();
     }
 
     public void render(){
+        GL11.glTranslatef(xScroll, yScroll, 0);
         level.render();
     }
 }
